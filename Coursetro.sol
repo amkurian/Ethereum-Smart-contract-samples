f601f9e985c64a3355b87e41748d8f8f655c75d0
Arun Mathew Kurian <amkurian@gmail.com>
2018-03-17 21:14:10 +0530

    Implementing Function modifiers

--- Coursetro.sol
+++ Coursetro.sol
@@ -4,16 +4,26 @@ contract Coursetro {
 
    string fName;
    uint age;
+   address owner;
+
+   function Coursetro() public {
+       owner = msg.sender;
+   }
+
+   modifier onlyOwner {
+       require(msg.sender == owner);
+       _;
+   }
 
    event Instructor(
-   string fName,
+   string name,
    uint age
-   )
+   );
 
-   function setInstructor(string _fName, uint _age) public {
+   function setInstructor(string _fName, uint _age) onlyOwner public {
        fName = _fName;
        age = _age;
-       Instructor(_fName, _age);
+       emit Instructor(_fName, _age);
    }
 
    function getInstructor() public constant returns (string, uint) {
